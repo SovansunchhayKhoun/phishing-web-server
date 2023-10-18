f7a7729fc46a91118ac4086e72decbf11c34a4e8
RUiNe4 <sunchhay@gmail.com>
2023-10-18 09:10:00 +0700

    config login

--- controller/authController.js
+++ controller/authController.js
@@ -26,20 +26,39 @@ const register = async (req, res) => {
   }
 };
 
+// const login = async (req, res) => {
+//   const { email, password } = req.body;
+//   let errors = [];
+//   if (!email) errors.push({ email: "Email is required!" });
+//   if (!password) errors.push({ password: "Password is required!" });
+//   if (errors.length > 0) return res.status(400).json({ errors });
+
+//   const matchEmail = await User.findOne({ email }).exec();
+//   if (!matchEmail) res.status(401).json({ message: "Email address does not exist." });
+//   else {
+//     const matchPassword = password === matchEmail.password;
+//     if (matchPassword)
+//       return res.status(200).json({ status: 200, message: "Login Successful" });
+//     return res.status(401).json({ message: "Invalid email or password." });
+//   }
+// };
+
 const login = async (req, res) => {
   const { email, password } = req.body;
   let errors = [];
   if (!email) errors.push({ email: "Email is required!" });
   if (!password) errors.push({ password: "Password is required!" });
   if (errors.length > 0) return res.status(400).json({ errors });
-
-  const matchEmail = await User.findOne({ email }).exec();
-  if (!matchEmail) res.status(401).json({ message: "Email address does not exist." });
   else {
-    const matchPassword = password === matchEmail.password;
-    if (matchPassword)
-      return res.status(200).json({ status: 200, message: "Login Successful" });
-    return res.status(401).json({ message: "Invalid email or password." });
+    return res.status(200).json({
+      status: 200,
+      message: "Login Successful",
+      user: {
+        username: email.split("@")[0],
+        email,
+        password,
+      },
+    });
   }
 };
 
